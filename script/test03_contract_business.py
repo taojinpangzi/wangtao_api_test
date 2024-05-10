b1c175d87f0988fc4f049f7538f8d98894be7f27
taojinpangzi <taojinwangzi@126.com>
2024-05-10 17:14:35 +0800

    modfiy testcase

--- script/test03_contract_business.py
+++ script/test03_contract_business.py
@@ -2,34 +2,29 @@ import pytest
 from api.login import LoginAPI
 
 
-# 创建测试类
 class TestContractBusiness:
     # 前置处理
-    @pytest.fixture()
-    def setup(self):
+    def setup_method(self):
         # 实例化接口对象
         self.login_api = LoginAPI()
 
     # 后置处理
-    @pytest.fixture()
-    def teardown(self):
+    def teardown_method(self):
         pass
 
     # 登陆成功
-    def test01_login_success(self, setup):
-        pass
-
+    def test01_login_success(self):
         # 获取验证码
         res_v = self.login_api.get_verify_code()
         print(res_v.status_code)
-        print(res_v.json())
+        print(res_v.json()["uuid"])
 
         # 登录
         login_data = {
             "username": "admin",
             "password": "HM_2023_test",
             "code": 2,
-            "uuid": "xxx"
+            "uuid": res_v.json()["uuid"]
         }
         res_l = self.login_api.login(test_data=login_data)
         print(res_l.status_code)
