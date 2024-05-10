160fa0e2e9c602593c8d3f41cbf4527f4bcdd599
taojinpangzi <taojinwangzi@126.com>
2024-05-10 18:25:23 +0800

    modfiy testcase

--- api/course.py
+++ api/course.py
@@ -1,10 +1,9 @@
 import requests
 
 
-class AddCourseAPI:
+class CourseAPI:
     def __init__(self):
         self.url_add_course = "http://kdtx-test.itheima.net/api/clues/course"
 
     def add_course(self, token, test_data):
         return requests.post(url=self.url_add_course, headers={"Authorization": token}, json=test_data)
-    
--- script/test03_contract_business.py
+++ script/test03_contract_business.py
@@ -1,12 +1,16 @@
 import pytest
 from api.login import LoginAPI
+from api.course import CourseAPI
 
 
 class TestContractBusiness:
+    token = None
+
     # 前置处理
     def setup_method(self):
         # 实例化接口对象
         self.login_api = LoginAPI()
+        self.add_course_api = CourseAPI()
 
     # 后置处理
     def teardown_method(self):
@@ -29,3 +33,17 @@ class TestContractBusiness:
         res_l = self.login_api.login(test_data=login_data)
         print(res_l.status_code)
         print(res_l.json())
+        TestContractBusiness.token = res_l.json()["token"]
+
+    # 新增课程成功
+    def test02_add_course_success(self):
+        add_data = {
+            "name": "测试开发提升课011111",
+            "subject": "6",
+            "price": 8991,
+            "applicablePerson": "2",
+            "info": "测试开发提升课01"
+        }
+        res_course = self.add_course_api.add_course(token=TestContractBusiness.token, test_data=add_data)
+        print(res_course.status_code)
+        print(res_course.json())
