436861080661e5874e87ecd8e34089550408bdc3
taojinpangzi <taojinwangzi@126.com>
2024-05-10 13:24:41 +0800

    Encapsulation verification code acquisition interface and login interface

--- api/login.py
+++ api/login.py
@@ -1,11 +1,14 @@
 import requests
 
+
 class LoginAPI:
+    # 初始化
     def __init__(self):
-        pass
+        self.url_verify = "http://kdtx-test.itheima.net/api/captchaImage"
+        self.url_login = "http://kdtx-test.itheima.net/api/login"
 
     def get_verify_code(self):
-        pass
+        return requests.get(url=self.url_verify)
 
-    def login(self):
-        pass
+    def login(self, test_data):
+        return requests.post(url=self.url_login, json=test_data)
